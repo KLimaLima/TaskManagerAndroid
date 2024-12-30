cf3e8ba6b51dc98bf9a10b1682e7c0dcdd3e609b
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2024-12-31 00:35:21 +0800

    additional dao stuff

--- app/src/main/java/com/lima/taskmanagerandroid/data/TaskDao.kt
+++ app/src/main/java/com/lima/taskmanagerandroid/data/TaskDao.kt
@@ -7,6 +7,7 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
 import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
@@ -20,9 +21,24 @@ interface TaskDao {
     @Query("SELECT * FROM task WHERE _id = :taskId")
     suspend fun getTaskById(taskId: Int): Task
 
+    @Transaction
+    @Query("SELECT * FROM task WHERE _id = :taskId")
+    fun getTaskByIdFlow(taskId: Int): Flow<Task?>
+
+    @Transaction
+    @Query("SELECT * FROM task ORDER BY isFinished DESC")
+    fun readAllTasksSortByFavorite(): Flow<List<Task>>
+
+    @Transaction
+    @Query("SELECT * FROM task")
+    fun readAllTasks(): Flow<List<Task>>
+
     //Update
     @Update
     suspend fun updateTask(task: Task)
+
+    @Query("UPDATE task SET isFinished = :isFinished WHERE _id = :taskId")
+    suspend fun setFinishedTask(isFinished: Boolean, taskId: Int)
 
     //Delete
     @Delete
